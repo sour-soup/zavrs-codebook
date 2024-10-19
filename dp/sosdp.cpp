d0c875f5c8cc21dd30523d39fae6f495691d7194
Kirill <fikfack806@mail.ru>
2024-10-19 20:37:05 +0400

    Добавил sos dp

---
+++ dp/sosdp.cpp
@@ -0,0 +1,11 @@
+// Given a fixed array A of 2N integers, we need to calculate ∀ x function F(x) = Sum of all A[i] such that x&i = i, i.e., i is a subset of x.
+for(int mask = 0; mask < (1<<N); ++mask){
+	dp[mask][-1] = A[mask];	//handle base case separately (leaf states)
+	for(int i = 0;i < N; ++i){
+		if(mask & (1<<i))
+			dp[mask][i] = dp[mask][i-1] + dp[mask^(1<<i)][i-1];
+		else
+			dp[mask][i] = dp[mask][i-1];
+	}
+	F[mask] = dp[mask][N-1];
+}
