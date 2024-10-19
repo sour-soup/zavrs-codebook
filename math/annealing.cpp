3efd0774aa17a67ff655660fbef8cae3440bd1e3
Vadim <vadimyanchenko64@gmail.com>
2024-10-19 13:55:49 +0400

    describe annealing

--- math/annealing.cpp
+++ math/annealing.cpp
@@ -1,17 +1,18 @@
-const ld t0 = 1e5; // or 1e9
+const ld t0 = 1e5; // or 1e9; init temp
 const ld chg = 0.99999;
 ld temp = t0;
-int cur = calc();
+int cur = random_state(); // any random valid state
 int ans = cur;
-while (temp causes updates || clock) {
+// find minimum of function f
+while (temp > END_TEMP && elapsed_time() <= time_limit) {
 	// save the current state
 	// make temp / t0 fraction of updates
-	int tmp = calc();
-	ans = min(ans, tmp);
-	if (tmp < cur) {
+	int next = neighbor(s);
+	if (func(ans) > func(next)) {
 		cur = tmp;
 	}
 	else{
+		// Probability to go to worse state
 		if (rnd_ld(0, 1) <= exp((cur - tmp) / temp))
 			cur = tmp;
 		else
