73c9333327969a56d2029fb9059baa23489ec8ca
Vadim <vadimyanchenko64@gmail.com>
2024-10-19 14:52:25 +0400

    Добавил дискретный лог

---
+++ nt/discreteLog.cpp
@@ -0,0 +1,15 @@
+int discreteLog(int a, int b, int m) {
+    a %= m, b %= m;
+    int n = sqrt(m) + 1;
+    map<int, int> vals;
+    for (int p = 1; p <= n; ++p)
+        vals[binpow(a, p * n, m)] = p;
+    for (int q = 0; q <= n; ++q) {
+        int cur = (binpow(a, q, m) * 1ll * b) % m;
+        if (vals.count(cur)) {
+            int ans = vals[cur] * n - q;
+            return ans;
+        }
+    }
+    return -1;
+}
