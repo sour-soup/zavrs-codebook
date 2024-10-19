2fc328eebd7a345ba0cd6f7af4303409334c6bf7
sour-soup <kiziyepi8@gmail.com>
2024-10-19 05:06:15 +0400

    cosmetic change exgcd

--- nt/exgcd.cpp
+++ nt/exgcd.cpp
@@ -1,11 +1,11 @@
-li exgcd (li a, li b, li& x, li& y) {
+ll exgcd(ll a, ll b, ll& x, ll& y) {
 	if (b == 0) {
 		x = 1, y = 0;
 		return a;
 	}
-	li x1, y1;
-	li g = exgcd(b, a % b, x1, y1);
+	ll x1, y1;
+	ll d = exgcd(b, a % b, x1, y1);
 	x = y1;
 	y = x1 - y1 * (a / b);
-	return g;
+	return d;
 }
