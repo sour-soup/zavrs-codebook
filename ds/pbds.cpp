5cf387abc91eb7821386c62add24eb3bf53e4b28
vadim <vadimyanchenko64@gmail.com>
2024-12-10 22:50:37 +0400

    fix: дополнил ordered_set

--- ds/pbds.cpp
+++ ds/pbds.cpp
@@ -3,6 +3,7 @@
 using namespace __gnu_pbds;
 
 gp_hash_table<ll, int> h({},{},{},{}, {1 << 16});
+// or less_equal<T>/pair<T,int> to get multiset!
 template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
 template <typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
 
