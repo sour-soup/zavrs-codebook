bec8d7b86a78f9bc4ee4225aab3d7866f981d00b
sour-soup <kiziyepi8@gmail.com>
2025-10-19 00:45:36 +0400

    fix: поправил декартач

--- ds/treap.cpp
+++ ds/treap.cpp
@@ -1,21 +1,31 @@
 struct node {
-	int y, val, cnt = 0, rev = 0;
+	int x, y, val, cnt;
+	ll sm = 0, add = 0;
 	node* l = 0;
 	node* r = 0;
-	node(int val) {
+	node(int x, int val) {
+		this->x = x;
 		this->val = val;
 		y = mt();
+		sm = val;
 		cnt = 1;
+		add = 0;
+		l = r = 0;
 	}
-	node() {}
+	node() {
+		val = 0;
+		x = y = 0;
+		cnt = 0;
+	}
 };
+
 using treap = node*;
- 
 const int N = 2e5 + 10;
 node buf[N];
 int siz = 0;
-treap new_node(int val) {
-	buf[siz] = node(val);
+
+treap new_node(int x, int val) {
+	buf[siz] = node(x, val);
 	return &buf[siz++];
 }
 node get(treap t) {
@@ -24,23 +34,25 @@ node get(treap t) {
 }
 treap fix(treap t) {
 	if (!t) return 0;
-	t->cnt = get(t->l).cnt + get(t->r).cnt + 1; 
+	t->cnt = get(t->l).cnt + get(t->r).cnt + 1;
+	t->sm = get(t->l).sm + get(t->r).sm + t->val + get(t->l).cnt * get(t->l).add + get(t->r).cnt * get(t->r).add;
 	return t;
 }
 treap push(treap t) {
 	if (!t) return 0;
-	if (t->rev) {
-		swap(t->l, t->r);
-		if (t->l) t->l->rev ^= 1;
-		if (t->r) t->r->rev ^= 1;
-		t->rev = 0;
+	if (t->add) {
+		t->val += t->add;
+		t->sm += t->add * t->cnt;
+		if (t->l) t->l->add += t->add;
+		if (t->r) t->r->add += t->add;
+		t->add = 0;
 	}
 	return t;
 }
 treap merge(treap a, treap b) {
 	if (!a) return b;
 	if (!b) return a;
-	a = push(a); b = push(b);
+	a = push(a), b = push(b);
 	if (a->y > b->y) {
 		a->r = merge(a->r, b);
 		return fix(a);
@@ -50,7 +62,23 @@ treap merge(treap a, treap b) {
 		return fix(b);
 	}
 }
-pair<treap, treap> split(treap t, int k) {
+
+pair<treap, treap> split(treap t, int x) {
+	if (!t) return { 0, 0 };
+	t = push(t);
+	if (t->x < x) {
+		auto p = split(t->r, x);
+		t->r = p.first;
+		return { fix(t), p.second };
+	}
+	else {
+		auto p = split(t->l, x);
+		t->l = p.second;
+		return { p.first, fix(t) };
+	}
+}
+
+pair<treap, treap> splitK(treap t, int k) {
 	if (!t) return { 0, 0 };
 	t = push(t);
 	int cntl = get(t->l).cnt;
